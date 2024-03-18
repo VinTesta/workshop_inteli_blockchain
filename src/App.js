f52094dda031f9dd3bb2301b03d8abf2c0fdf47b
VinTesta <viniciustestapassos@gmail.com>
2024-03-18 09:11:19 -0300

    fix: adição de alt nas imagens

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import "./App.css";
 import logo from "./assets/logo.svg";
 import Input from "./components/Input/Input";
+import github_icon from "./assets/github.svg";
+import wpp_icon from "./assets/wpp.svg";
 
 const SITE_URL = "https://www.inteliblockchain.co/";
+const WPP_URL = "https://chat.whatsapp.com/EoP5wbGPujg8vpG9f6KNWz";
+const URL_GITHUB = "https://github.com/VinTesta/workshop_inteli_blockchain";
 
 function App() {
   console.log(process.env);
@@ -11,7 +15,8 @@ function App() {
       <div className="flex justify-center flex-col items-center p-4 w-full">
         <a
           href={SITE_URL}
-          target="_blank"
+          target="_blank" 
+          rel="noreferrer"
           className="flex justify-center items-center w-full md:w-2/3 lg:w-2/3"
         >
           <img
@@ -23,7 +28,10 @@ function App() {
           />
         </a>
 
-        <div className="m-4 column-md-4 column-sm-12 bg-white bg-opacity-20 text-white rounded-lg py-8 px-4 shadow-lg w-full w-5/6 md:w-2/3 lg:w-1/2 xl:w-1/3">
+        <div 
+          className="m-4 column-md-4 column-sm-12 bg-white bg-opacity-20 
+            text-white rounded-lg py-8 px-4 shadow-lg w-full w-5/6 md:w-2/3 lg:w-1/2 xl:w-1/3
+        ">
           <h1 className="text-xl md:text-2xl">
             Receba sua Proof-of-Attandance NFT
           </h1>
@@ -32,20 +40,59 @@ function App() {
           </h5>
           <h5 className="mt-4 mb-4 text-sm">
             Ainda não tem uma carteira? Acesse o link e crie a sua já: 
-            <a className="text-blue-500 hover:underline ms-2" href="https://metamask.io/download/">
+            <a 
+              target="_blank" 
+              rel="noreferrer" 
+              className="text-blue-500 hover:underline ms-2" 
+              href="https://metamask.io/download/
+            ">
               Clique aqui.
             </a>
           </h5>
           <form action="" method="POST" className="flex flex-col gap-4">
-            <Input label="Nome" inputDescription="Esse nome será gravado no NFT para provar sua presença" placeholder="Nome" />
-            <Input label="E-mail" inputDescription="Email para receber novidades" placeholder="your.email@email.com" />
-            <Input label="Wallet" inputDescription="Carteira para receber o NFT na rede Sepolia" placeholder="0x00...0000" />
-            <button className="text-white bg-blue-500 text-xl p-2 rounded-md shadow-md disabled:bg-gray-300 disabled:cursor-not-allowed">Gerar NFT</button>
+            <Input 
+              label="Nome" 
+              inputDescription="Esse nome será gravado no NFT para provar sua presença" 
+              placeholder="Nome" />
+            <Input 
+              label="E-mail" 
+              inputDescription="Email para receber novidades" 
+              placeholder="your.email@email.com" />
+            <Input 
+              label="Wallet" 
+              inputDescription="Carteira para receber o NFT na rede Sepolia" 
+              placeholder="0x00...0000" />
+            <button 
+              className="text-white bg-blue-500 text-xl p-2 
+                rounded-md shadow-md disabled:bg-gray-300 disabled:cursor-not-allowed
+              ">
+              Gerar NFT
+            </button>
           </form>
         </div>
       </div>
-      <p className="text-center w-full text-gray-400 text-sm">Todo o código pode ser encontrado aqui: </p>
-      <p className="text-center w-full text-gray-400 text-sm">Caso tenha alguma dúvida: </p>
+      <a 
+        className="flex flex-row justify-center items-center" 
+        target="_blank" 
+        rel="noreferrer" 
+        href={URL_GITHUB}
+      >
+        <p className="text-center w-full text-gray-400 text-sm me-2">
+          Todo o código pode ser encontrado aqui: 
+        </p>
+        <img width={15} alt="Icone do github"src={github_icon}/>
+      </a>
+      <a 
+        className="flex flex-row justify-center items-center" 
+        target="_blank" 
+        rel="noreferrer" 
+        href={WPP_URL}
+      >
+        <p className="text-center w-full text-gray-400 text-sm me-2">
+          Caso tenha alguma dúvida: 
+        </p>
+        <img width={15} alt="Icone do wpp" src={wpp_icon}/> 
+      </a>
     </div>
   );
 }
